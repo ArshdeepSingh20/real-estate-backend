969616ffa8fc7f03a58ebdc626ff6f0c560f91e0
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-04-15 23:09:56 +0530

    completed signin signup controller, routes and created separate errorHandler to handle error

--- src/app.js
+++ src/app.js
@@ -1,24 +1,38 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 // import cookieParser from "cookie-parser"
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-// app.use(express.json({limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }));
 // app.use(express.urlencoded({extended: true, limit: "16kb"}))
 // app.use(express.static("public"))
 // app.use(cookieParser())
 
-
 //routes import
-
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 //routes declaration
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
 
-export { app }
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+export { app };
