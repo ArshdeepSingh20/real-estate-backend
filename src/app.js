4d2767ddceb0450954c511b9c63ec6da56846661
Arshdeep Singh <singharshdeep9039@gmail.com>
2024-04-18 12:35:36 +0530

    completing create listing successfully

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -12,17 +12,19 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 // app.use(express.static("public"))
-app.use(cookieParser())
+app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/listings", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
